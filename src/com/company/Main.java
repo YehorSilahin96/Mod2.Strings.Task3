5adb9282eaae76bdd18dd780f397f5bc6b5c69b6
AnnaDremluga <dreanna3@gmail.com>
2021-06-16 22:54:57 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String a = "Hello!";
-        String b = new String("Help");
+        String a = "Hello!"; // pay attention to naming, it mustn't be 1 letter
+        String b = new String("Help"); // this variable must be the same - "Hello!". Try with it and see the result 
         String c = a.intern();
 
         System.out.println(a == b);
